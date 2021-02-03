de6f95de7928173ae6407c9f4dea71ff6f5eba24
ahchambers <66989949+ahchambers@users.noreply.github.com>
2021-02-03 12:49:33 +1100

    update

--- UFO-level/app.js
+++ UFO-level/app.js
@@ -20,18 +20,12 @@ data.forEach((sighting) => {
 }
 
 
-// Select button
-var button = d3.select("#filter-btn");
-
-// Select date input 
-var inputField = d3.select("#example-date-input");
-
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit", runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
+
+  // Declare filteredData
+   let filteredData = tableData;
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
@@ -40,8 +34,8 @@ function runEnter() {
   var inputElement = d3.select("#example-date-input");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
+  var date = inputElement.property("value");
+  
   //Apply filter
   if (date) {
     filteredData = filteredData.filter(row => row.datetime === date);
@@ -51,7 +45,7 @@ function runEnter() {
   }
 
   // Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", runEnter);
 
 // Build the table when the page loads
 buildTable(tableData);
